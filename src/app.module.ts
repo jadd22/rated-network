d62a6b5a9e411c0c960a15b59a40b3ef04702f53
Jaydeep Chauhan <jaydeeplc@hotmail.com>
2024-06-15 15:42:34 +0530

    sample api integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NetworkStatsModule } from './network-stats/network-stats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    NetworkStatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
